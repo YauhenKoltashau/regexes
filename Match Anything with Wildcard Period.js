d0fe4c096a729f820612576f0c871ce24768e666
YauhenKoltashau <yauhen.koltashau.1985@gmail.com>
2023-05-29 00:03:12 +0200

    regexes3

---
+++ Match Anything with Wildcard Period.js
@@ -0,0 +1,4 @@
+let exampleStr = "Let's have fun with regular expressions!";
+let unRegex = /run.|sun.|fun.|pun.|nun.|bun./; // Change this line
+let result = unRegex.test(exampleStr);
+console.log(result);
