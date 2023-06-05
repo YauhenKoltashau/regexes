704af62aa2c3ef3777ce3b8d475a65ae296d12e7
YauhenKoltashau <yauhen.koltashau.1985@gmail.com>
2023-06-05 23:30:25 +0200

    regexes7

--- Restrict Possible Usernames.js
+++ Restrict Possible Usernames.js
@@ -1,6 +1,6 @@
 let username = "JackOfAllTrades10";
-// let userCheck = /^\D\w+\d$/g; // Change this line
-let userCheck = /^[a-z]([a-z]+\d*|\d{2,})$/gi; // Change this line
+let userCheck = /^[a-z]([a-z]+\d*)$/i; // Change this line
+let userCheck2 = /^[a-z]([a-z]+\d*|\d{2,})$/i; // Change this line
 let resultItems = username.match(userCheck)
 
 let result = userCheck.test(username);
